7a3b8c814e1e27aa41f0388b572fbfcec354ef6d
Jake <jsaquisame@gmail.com>
2018-11-15 18:21:05 -0600

    applied the session on to the project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,16 @@ class ApplicationController < ActionController::Base
     private 
     def initialize_session
      session[:visit_count] ||= 0
+     session[:favourite_song] ||= []
     end
 
     def load_and_increment_visit_count
         @visit_count = session[:visit_count] + 1
         session[:visit_count] = @visit_count
     end
+
+    def load_playlist
+        @favourite_song  = Song.find(session[:favourite_song])
+    end
+
 end
